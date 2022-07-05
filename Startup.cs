66f89ce81aa1f75988f110ee2acd2af7d8e3f6ca
Daniel Melo Olarte <dmeloo@hotmail.com>
2022-07-05 14:45:14 -0500

    None

--- Startup.cs
+++ Startup.cs
@@ -70,7 +70,7 @@ namespace MvcCode
                 {
                     options.Cookie.Name = CookieAuthenticationDefaults.CookiePrefix + CookieAuthenticationDefaults.AuthenticationScheme;
                     options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-                    options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Lax;
+                    options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.None;
                 })
                 .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
                 {
